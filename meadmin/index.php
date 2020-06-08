f23f9d43cdd3d4788661ff97e48cf76e88124a7f
ashikrichard <ashikrichard@gmail.com>
2020-06-08 09:23:16 -0700

    initial commit

---
+++ meadmin/index.php
@@ -0,0 +1,125 @@
+<?php 
+session_start();
+if(@$_SESSION['meadmin'])
+{
+	header("location:home.php");
+}
+ include "../setting/config.php";
+
+
+
+if(isset($_POST['admin_signin']))
+{
+	
+	$admin_username = $_POST['admin_username'];
+	$admin_password = $_POST['admin_password'];
+	$t_staff_type = $_POST['t_staff_type'];
+	
+	if($admin_username=="" AND $admin_password=="")
+	{
+		echo "<script>alert('Enter Your Username & Password');</script>";
+	}
+	else
+	{
+		$melogin = $ravi->meadmin_check($admin_username,$admin_password,$t_staff_type);
+			if($melogin==1)
+	{
+	$_SESSION['meadmin'] = 	$admin_username;
+		header("location:home.php");
+	}
+		
+		
+	else
+	{
+		echo "<script>alert('Sorry Try Later');</script>";
+	}
+	
+	}
+
+	
+}
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>NTTF</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="NTTF Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+<script src="js/jquery-1.10.2.min.js"></script>
+<!--clock init-->
+</head> 
+<body>
+								<!--/login-->
+								
+									   <div class="error_page">
+												<!--/login-top-->
+												
+													<div class="error-top">
+													
+														<h2 class="inner-tittle page">Login</h2>
+													
+													    <div class="login">
+															<h3 class="inner-tittle t-inner">RR E-Learning House Login</h3>
+														
+																<div class="buttons login">
+																			<ul>
+																				<li><a href="#" class="hvr-sweep-to-right">Facebook</a></li>
+																				<li class="lost"><a href="#" class="hvr-sweep-to-left">Twitter</a> </li>
+																				<div class="clearfix"></div>
+																			</ul>
+																		</div>
+																<form method="post">
+																
+																		<input type="text" class="text" name="admin_username" placeholder="Username">
+																		<input type="password" placeholder="Password" name="admin_password">
+																		<div class="submit"><input type="submit" value="Login" name="admin_signin"></div>
+																		<div class="clearfix"></div>
+																		
+																		<div class="new">
+																			<p><label class="checkbox11"><input type="checkbox" name="checkbox"><i> </i>Forgot Password ?</label></p>
+																			
+																			<div class="clearfix"></div>
+																		</div>
+																	</form>
+														</div>
+
+														
+													</div>
+													
+													
+												<!--//login-top-->
+										   </div>
+						
+										  	<!--//login-->
+										    <!--footer section start-->
+										<div class="footer">
+												<div class="error-btn">
+															<a class="read fourth" href="index.html">Return to Home</a>
+															</div>
+										   <p>Developed By 2k17 Batch</p>
+										</div>
+									<!--footer section end-->
+									<!--/404-->
+<!--js -->
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
